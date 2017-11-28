3433d9f3098ce147b5d1ec0a94024d79c23e8af8
maji-KY <maji-ky@neco-labo.com>
2017-11-28 19:00:24 +0900

    interprets params datetime as local midnight datedime

--- src/main/scala/org/embulk/parser/twitter_ads_stats/define/Request.scala
+++ src/main/scala/org/embulk/parser/twitter_ads_stats/define/Request.scala
@@ -1,6 +1,6 @@
 package org.embulk.parser.twitter_ads_stats.define
 
-import java.time.{LocalDate, LocalDateTime}
+import java.time.LocalDate
 
 case class Request(
     params: Params
@@ -11,14 +11,15 @@ object Request {
 }
 
 case class Params(
-    start_time: LocalDateTime,
-    end_time: LocalDateTime,
+    start_time: StatsDateTime,
+    end_time: StatsDateTime,
     placement: String
 ) {
-  require(!start_time.isAfter(end_time))
+  require(!start_time.utcDateTime.isAfter(end_time.utcDateTime))
+  require(start_time.utcDateTime.toLocalTime == end_time.utcDateTime.toLocalTime)
 
-  val startDate = start_time.toLocalDate
-  val endDate   = end_time.toLocalDate
+  val startDate = start_time.adAccountLocalDate
+  val endDate   = end_time.adAccountLocalDate
 
   /**
     * MetricTimeSeries の期間
--- src/test/scala/org/embulk/parser/twitter_ads_stats/define/ParamsSpec.scala
+++ src/test/scala/org/embulk/parser/twitter_ads_stats/define/ParamsSpec.scala
@@ -1,12 +1,12 @@
 package org.embulk.parser.twitter_ads_stats.define
 
-import java.time.{LocalDate, LocalDateTime}
+import java.time.LocalDate
 
 import org.embulk.parser.twitter_ads_stats.UnitSpec
 
 class ParamsSpec extends UnitSpec {
   "開始日~(終了日-1)の日程を取得する" in {
-    val period = Params(LocalDateTime.of(2017, 1, 1, 0, 0, 0), LocalDateTime.of(2017, 1, 4, 23, 23, 23), "")
+    val period = Params(StatsDateTime("2016-12-31T15:00:00Z"), StatsDateTime("2017-01-03T15:00:00Z"), "")
     val actual = period.targetDates
     val expected = List(
       LocalDate.of(2017, 1, 1),
@@ -16,7 +16,7 @@ class ParamsSpec extends UnitSpec {
     assert(actual == expected)
   }
   "開始日から終了日までの全日程は、開始日から終了日が同一な場合は同一な日程となる" in {
-    val period = Params(LocalDateTime.of(2017, 1, 1, 0, 0, 0), LocalDateTime.of(2017, 1, 1, 23, 23, 23), "")
+    val period = Params(StatsDateTime("2016-12-31T15:00:00Z"), StatsDateTime("2017-01-01T15:00:00Z"), "")
     val actual = period.targetDates
     val expected = List(
       LocalDate.of(2017, 1, 1)
